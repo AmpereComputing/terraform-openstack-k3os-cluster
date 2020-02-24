997f7f98194a359c42509ff7a60318058b12e307
Peter Pouliot <peter@pouliot.net>
2020-02-24 07:18:37 -0800

    changing permissions via local_file vs an exec on the key after

--- main.tf
+++ main.tf
@@ -12,26 +12,29 @@ resource "tls_private_key" "k3os" {
 resource "local_file" "k3os-ssh-privkey" {
     content = tls_private_key.k3os.private_key_pem
     filename = "${path.module}/k3os-id_rsa"
+    file_permission = "0600"
 }
 
 resource "local_file" "k3os-ssh-pubkey" {
     content  = tls_private_key.k3os.public_key_openssh
     filename = "${path.module}/k3os-id_rsa.pub"
+    file_permission = "0644"
 }
 
 output "k3os_ssh_pubic_key" {
-  value = "${tls_private_key.k3os.public_key_openssh}"
+  value = tls_private_key.k3os.public_key_openssh
 }
 
 output "k3os_ssh_private_key" {
-  value = "${tls_private_key.k3os.private_key_pem}"
+  value = tls_private_key.k3os.private_key_pem
 }
 
 
 resource "random_uuid" "cluster" { }
 
-resource "null_resource" "key_permissions" {
-  provisioner "local-exec" {
-    command = "chmod 0600 ${path.module}/k3os-id_rsa"
-  }
-}
+# Removing due to adding permissions to the local_file
+#resource "null_resource" "key_permissions" {
+#  provisioner "local-exec" {
+#    command = "chmod 0600 ${path.module}/k3os-id_rsa"
+#  }
+#}
